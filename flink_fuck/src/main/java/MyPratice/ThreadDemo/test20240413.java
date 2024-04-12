6e2b434b8d7d0ffa37c677bb39965c494685a91a
Ti-yuan <tiyuan8888@163.com>
2024-04-13 00:05:04 +0800

    1

---
+++ flink_fuck/src/main/java/MyPratice/ThreadDemo/test20240413.java
@@ -0,0 +1,12 @@
+package MyPratice.ThreadDemo;
+
+/**
+ * @AUTHOR: Maynard
+ * @DATE: 2024/04/13 00:02
+ **/
+
+public class test20240413 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
